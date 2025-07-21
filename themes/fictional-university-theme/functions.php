41c431873dcbcdd72e782621baf17cb5c2a62f69
Chandan Kumar <93089403+chandan578@users.noreply.github.com>
2025-07-21 22:33:32 +0530

    Update functions.php

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -102,7 +102,7 @@ function university_adjust_queries($query) {
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api){
-  $api['key'] = 'AIzaSyB124UVlNL6MZS6C21X4zuFa2q579Bgg04';
+  $api['key'] = 'map_api_key_used_here';
   return $api;
 }
 
